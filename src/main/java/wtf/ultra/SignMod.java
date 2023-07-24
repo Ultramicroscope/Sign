c0c07a6add96fbac8990f3e3526472c64fc86990
Ryan <ryan@ultra.wtf>
2023-07-24 13:23:44 -0400

    refactored - uses stream api now (morbidly a beast)

--- src/main/java/wtf/ultra/SignMod.java
+++ src/main/java/wtf/ultra/SignMod.java
@@ -10,46 +10,26 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallba
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.impl.event.lifecycle.LoadedChunksCache;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.chunk.WorldChunk;
 
 import java.util.*;
 
 public class SignMod implements ModInitializer {
-
+    private final Gson GSON = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
     private final Map<BlockPos, String[]> signs = new TreeMap<>();
 
     @Override
     public void onInitialize() {
-        ClientTickEvents.END_WORLD_TICK.register(world -> {
-            Set<WorldChunk> loadedChunks = ((LoadedChunksCache) world).fabric_getLoadedChunks();
-            Set<BlockEntity> blockEntities = new HashSet<>();
-            loadedChunks.forEach(chunk -> blockEntities.addAll(chunk.getBlockEntities().values()));
-            blockEntities.forEach(blockEntity -> {
-                if (blockEntity.getType() == BlockEntityType.SIGN) {
-                    SignBlockEntity sign = (SignBlockEntity) blockEntity;
-                    Text[] signText = sign.getText(true).getMessages(false);
-                    String[] text = new String[4];
-                    for (int i = 0; i < 4; i++) text[i] = signText[i].getString();
-                    signs.put(sign.getPos().toImmutable(), text);
-                }
-            });
-        });
-
+        ClientTickEvents.END_WORLD_TICK.register(world -> ((LoadedChunksCache) world).fabric_getLoadedChunks().stream().flatMap(chunk -> chunk.getBlockEntities().values().stream()).filter(blockEntity -> blockEntity instanceof SignBlockEntity).map(blockEntity -> (SignBlockEntity) blockEntity).forEach(sign -> signs.put(sign.getPos().toImmutable(), Arrays.stream(sign.getText(true).getMessages(false)).map(Text::getString).toArray(String[]::new))));
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> signs.clear());
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("dump_signs").executes(context -> {
-            Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
-
             JsonObject jsonObject = new JsonObject();
-            signs.forEach((pos, arr) -> jsonObject.add(pos.toShortString().replace(",", ""), gson.toJsonTree(arr)));
-            System.out.println(gson.toJson(jsonObject));
+            signs.forEach((pos, arr) -> jsonObject.add(pos.toShortString().replace(",", ""), GSON.toJsonTree(arr)));
+            System.out.println(GSON.toJson(jsonObject));
 
             return 1;
         })));
-
-        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> signs.clear());
     }
 }
